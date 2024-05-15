fe79d565f7880be3c550ea6b24333976cccc620a
PMinhVu <ITITIU20354@student.hcmiu.edu.vn>
2024-05-16 05:25:54 +0700

    chore: update jsconfig file to ctrl+click can navigate to true file

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,12 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-          '@components': path.resolve(__dirname, './src/components'),
-          '@pages': path.resolve(__dirname, './src/pages'),
-          '@assets': path.resolve(__dirname, './src/assets')
-        },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@assets': path.resolve(__dirname, './src/assets')
     },
+  },
 });
